38374dd3ce6677de1f68dd1b3653eff3064107f1
Jv <89169485+Jotave1@users.noreply.github.com>
2024-03-06 00:34:18 -0300

    Add files via upload

---
+++ Algoritmos/Lista 1/output/Questao1.cpp
@@ -0,0 +1,31 @@
+/*Leia a idade de uma pessoa expressa em anos, meses e dias e mostre-a expressa apenas em dias. Obs: Considere cada mês com 30 dias.*/
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  /*
+    ano = inteiro que guarda a quantidade de dias em 1 ano
+    mes = inteiro que guarda a quantidade de dias em 1 mes
+    an0 = inteiro que guarda a quantidade de anos dita pelo usuario
+    me5 = inteiro que guaarda a quantidade de meses dita pelo usuario
+    dia = inteiro que guarda a quantidade de dias dita pelo usuario
+  */
+  int an0 = 0, me5 = 0, dia = 0;
+
+  cout << "\nDiga sua idade em anos, meses e dias: ";
+  cin >> an0 >> me5 >> dia;
+
+  if(an0 <= 0){
+    cout << "\nErro: anos invalidos";
+
+  }else if(me5 >= 12 || me5 < 0){
+      cout << "\nErro: meses invalidos";
+
+  }else if(dia < 0){
+    cout << "\nErro: dias invalidos";
+
+  }else{  
+    cout << "\nSua idade em dias é: " << (an0 * 365) + (me5 * 30) + dia << " dias";
+  }
+}
